9863c4b074845890c0a3e04aa0754e558ad30ec1
Nishita <nishitasingh022@gmail.com>
2024-04-11 19:31:51 +0530

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<BrowserRouter>
+<Routes>
+  <Route path='/' element={<Home/>}/>
+  <Route path='/about' element={<About/>}/>
+  <Route path='/sign-in' element={<Signin/>}/>
+  <Route path='/sign-up' element={<Signup/>}/>
+  <Route path='/dashboard' element={<Dashboard/>}/>
+  <Route path='/projects' element={<Projects/>}/>
+</Routes>
+</BrowserRouter>
   );
 }
 
