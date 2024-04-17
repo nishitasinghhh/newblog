1a7a2db080fe36084ee8cdd479994d3196477b18
Nishita <nishitasingh022@gmail.com>
2024-04-17 11:22:17 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -22,30 +22,32 @@ next(error)
    }
    
 }
+
 export const signin=async(req,res,next)=>{
    const {email,password}=req.body;
-   if(!email || !password || email==="" || password==="")
-   {
-      next(errorHandler(404,'All fields are required'));
+   if(!email || !password || email==='' || password===''){
+      next(errorHandler(400,'All fields are required'));
    }
+   
    try{
       const validUser= await User.findOne({email})
       if(!validUser)
       {
-         next(errorHandler(404,'User not found'))
+         return next(errorHandler(404,'User not found'))
       }
       const validPassword=bcryptjs.compareSync(password,validUser.password)
       if(!validPassword)
       {
-         next(errorHandler(400,'Invalid Password'))
+        return next(errorHandler(400,'Invalid Password'))
       }
       const token=jwt.sign(
          {
             id:validUser._id
          },
-         process.env.JWT_SECRET
+         process.env.JWT_SECRET,
       )
-      res.status(200).cookie('access_token',token,{httpOnly:true}).json(validUser)
+      const {password:pass,...rest}=validUser._doc
+      res.status(200).cookie('access_token',token,{httpOnly:true}).json(rest)
    } catch(error)
    {
       next(error)
